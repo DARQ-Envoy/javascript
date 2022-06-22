610854ebd84d827b3b902c34f3432b72b96d84f9
DARQ-Envoy <nwokejitimoth00@gmail.com>
2022-06-22 09:08:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const body = document.querySelector("body");
+console.log(body);
+const div = document.createElement("div");
+div.innerHTML=`   
+ <ul>
+<li>Timothy Nwokeji</li>
+<li>6 feet</li>
+<li>Nigeria</li>
+</ul>
+
+        `
+body.appendChild(div)
